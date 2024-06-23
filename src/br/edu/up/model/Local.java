1275ecbbf9ed78147e5af47998442590e406e0d1
Gabriel Lincon <glincon44@gmail.com>
2024-06-23 19:25:40 -0300

    commit final

---
+++ src/br/edu/up/model/Local.java
@@ -0,0 +1,18 @@
+package br.edu.up.model;
+
+public class Local {
+    private String endereco;
+
+    public Local(String endereco) {
+        this.endereco = endereco;
+    }
+
+    // Getters e Setters
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
---
+++ src/br/edu/up/model/Organizador.java
@@ -0,0 +1,18 @@
+package br.edu.up.model;
+
+public class Organizador {
+    private String nome;
+
+    public Organizador(String nome) {
+        this.nome = nome;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
